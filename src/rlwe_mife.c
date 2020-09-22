063cdcf749a4fb03f4cc9dcf485ad17fbe81adde
Jose Maria Bermudo Mera <jbermudo@esat.kuleuven.be>
2020-09-22 03:07:37 +0200

    add functions supporting matrix-vector inner product

--- src/rlwe_mife.c
+++ src/rlwe_mife.c
@@ -155,6 +155,124 @@ rlwe_mife_decrypt
 }
 
 void
+rlwe_mife_encrypt_vec
+(uint32_t m[MIFE_L][MIFE_N], const uint32_t mpk[MIFE_L+1][MIFE_NMODULI][MIFE_N], uint32_t c[MIFE_L+1][MIFE_NMODULI][MIFE_N])
+{
+	int i, j, k;
+	uint64_t r[MIFE_N], f[MIFE_N];
+	uint32_t r_crt[MIFE_NMODULI][MIFE_N], f_crt[MIFE_NMODULI][MIFE_N];
+
+	uint32_t m_crt[MIFE_L][MIFE_NMODULI][MIFE_N];
+
+	uint64_t mxm;
+	for (i = 0; i < MIFE_L; ++i) {
+		crt_convert(m[i], m_crt[i]);
+		for (j = 0; j < MIFE_NMODULI; ++j) {
+			for (k = 0; k < MIFE_N; ++k) {
+				mxm = (uint64_t)m_crt[i][j][k] * MIFE_SCALE_M_MOD_Q_I[j];
+				m_crt[i][j][k] = mod_red(mxm, MIFE_MOD_Q_I[j]);
+			}
+		}
+	}
+
+	// Sample r, f_0 from D_sigma2
+	sample_sigma2(r);
+	sample_sigma2(f);
+	//sample_zeros(f);
+	// c_0 = a * r + f_0
+	crt_convert(r, r_crt);
+	crt_convert(f, f_crt);
+	for (i = 0; i < MIFE_NMODULI; ++i) {
+		poly_mul_mod(mpk[MIFE_L][i], r_crt[i], c[MIFE_L][i], MIFE_MOD_Q_I[i]);
+		add_mod(c[MIFE_L][i], f_crt[i], c[MIFE_L][i], MIFE_MOD_Q_I[i]);
+	}
+
+	// Sample f_i with i = 1...l from D_sigma3
+	// c_i = pk_i * r + f_i + (floor(q/p)m_i)1_R
+	for (i = 0; i < MIFE_L; ++i) {
+		sample_sigma3(f);
+		//sample_zeros(f);
+		crt_convert(f, f_crt);
+		for (j = 0; j < MIFE_NMODULI; ++j) {
+			poly_mul_mod(mpk[i][j], r_crt[j], c[i][j], MIFE_MOD_Q_I[j]);
+			add_mod(c[i][j], f_crt[j], c[i][j], MIFE_MOD_Q_I[j]);
+			add_mod(c[i][j], m_crt[i][j], c[i][j], MIFE_MOD_Q_I[j]);
+			//for (int k = 0; k < MIFE_N; ++k) {
+			//	c[i][j][k] = mod_red((c[i][j][k] + m_crt[j][i]), MIFE_MOD_Q_I[j]);
+			//}
+		}
+	}
+}
+
+void
+rlwe_mife_keygen_vec
+(const uint32_t y[MIFE_L][MIFE_N], const uint32_t msk[MIFE_L][MIFE_NMODULI][MIFE_N], uint32_t sk_y[MIFE_NMODULI][MIFE_N])
+{
+	int i, j, k;
+	uint64_t mac;
+
+	uint32_t y_crt[MIFE_L][MIFE_NMODULI][MIFE_N];
+
+	for (i = 0; i < MIFE_NMODULI; ++i) {
+		for (j = 0; j < MIFE_N; ++j) {
+			sk_y[i][j] = 0;
+		}
+	}
+
+	for (i = 0; i < MIFE_L; ++i) {
+		crt_convert(y[i], y_crt[i]);
+		for (j = 0; j < MIFE_NMODULI; ++j) {
+			poly_mul_mac_mod(y_crt[i][j], msk[i][j], sk_y[j], MIFE_MOD_Q_I[j]);
+			//for (k = 0; k < MIFE_N; ++k) {
+			//	mac = (uint64_t)y_crt[j][i]*msk[i][j][k];
+			//	mac = mac + sk_y[j][k];
+			//	sk_y[j][k] = mod_red(mac, MIFE_MOD_Q_I[j]);
+			//}
+		}
+	}
+}
+
+void
+rlwe_mife_decrypt_vec
+(const uint32_t c[MIFE_L+1][MIFE_NMODULI][MIFE_N], const uint32_t y[MIFE_L][MIFE_N], const uint32_t sk_y[MIFE_NMODULI][MIFE_N], uint64_t dy[MIFE_N])
+{
+	int i, j, k;
+	uint64_t mac;
+
+	uint32_t c0sy[MIFE_NMODULI][MIFE_N];
+	uint32_t d_y[MIFE_NMODULI][MIFE_N];
+
+	uint32_t y_crt[MIFE_L][MIFE_NMODULI][MIFE_N];
+
+	crt_convert_generic(y, y_crt, MIFE_L);
+
+	for (i = 0; i < MIFE_NMODULI; ++i) {
+		for (j = 0; j < MIFE_N; ++j) {
+			d_y[i][j] = 0;
+		}
+	}
+
+	for (i = 0; i < MIFE_L; ++i) {
+		crt_convert(y[i], y_crt[i]);
+		for (j = 0; j < MIFE_NMODULI; ++j) {
+			poly_mul_mac_mod(y_crt[i][j], c[i][j], d_y[j], MIFE_MOD_Q_I[j]);
+			//for (k = 0; k < MIFE_N; ++k) {
+			//	mac = (uint64_t)y_crt[j][i]*c[i][j][k];
+			//	mac = mac + d_y[j][k];
+			//	d_y[j][k] = mod_red(mac, MIFE_MOD_Q_I[j]);
+			//}
+		}
+	}
+
+	for (i = 0; i < MIFE_NMODULI; ++i) {
+		poly_mul_mod(c[MIFE_L][i], sk_y[i], c0sy[i], MIFE_MOD_Q_I[i]);
+		sub_mod(d_y[i], c0sy[i], d_y[i], MIFE_MOD_Q_I[i]);
+	}
+
+	crt_reverse(dy, d_y);
+}
+
+void
 round_extract
 (uint64_t a[MIFE_N])
 {
--- src/rlwe_mife.h
+++ src/rlwe_mife.h
@@ -11,6 +11,12 @@ void rlwe_mife_keygen (const uint32_t y[MIFE_L], const uint32_t msk[MIFE_L][MIFE
 
 void rlwe_mife_decrypt (const uint32_t c[MIFE_L+1][MIFE_NMODULI][MIFE_N], const uint32_t y[MIFE_L], const uint32_t sk_y[MIFE_NMODULI][MIFE_N], uint64_t dy[MIFE_N]);
 
+void rlwe_mife_encrypt_vec (uint32_t m[MIFE_L][MIFE_N], const uint32_t mpk[MIFE_L+1][MIFE_NMODULI][MIFE_N], uint32_t c[MIFE_L+1][MIFE_NMODULI][MIFE_N]);
+
+void rlwe_mife_keygen_vec (const uint32_t y[MIFE_L][MIFE_N], const uint32_t msk[MIFE_L][MIFE_NMODULI][MIFE_N], uint32_t sk_y[MIFE_NMODULI][MIFE_N]);
+
+void rlwe_mife_decrypt_vec (const uint32_t c[MIFE_L+1][MIFE_NMODULI][MIFE_N], const uint32_t y[MIFE_L][MIFE_N], const uint32_t sk_y[MIFE_NMODULI][MIFE_N], uint64_t dy[MIFE_N]);
+
 void round_extract (uint64_t a[MIFE_N]);
 
 #endif
